c0f637079133c700efc5a3f979eece1fe66baafb
Rafael Delgado <rafael.delgado.cubilla@iesjandula.es>
2020-10-07 20:33:14 +0200

    Añadida segunda Card con un FadeInImage y un texto debajo. Fin del vídeo 73.

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -10,7 +10,8 @@ class CardPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(20.0),
         children: [
-          _cardTipo1()
+          _cardTipo1(),
+          _cardTipo2(),
         ],
       )
     );
@@ -40,6 +41,26 @@ class CardPage extends StatelessWidget {
           )
         ],
       )
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: [
+          FadeInImage(
+            image: NetworkImage('https://iso.500px.com/wp-content/uploads/2014/06/W4A2827-1-3000x2000.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            //height: 400,
+            fit: BoxFit.fill
+            ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('No sé lo que poner'),
+          )
+        ],
+      ),
     );
   }
 }
